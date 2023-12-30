14ca89fe14a5c9f133099ecfea34811d5469d82c
Krishna-windows <krshnabiswal619@gmail.com>
2023-12-30 21:35:38 +0530

    Remove eslint-disable and import Link in Account.jsx

--- frontend/src/components/Others/Works/accountselect/Account.jsx
+++ frontend/src/components/Others/Works/accountselect/Account.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable react/prop-types */
 import classes from "./account.module.css";
 import { Link } from "react-router-dom";
+
 function Account({ data }) {
   return (
     <div className={classes.card}>
