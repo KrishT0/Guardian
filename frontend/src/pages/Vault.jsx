3ea170ef9a9eebdefebc0fe33ec33c2f159bc9a1
Krishna-windows <krshnabiswal619@gmail.com>
2024-01-04 12:57:27 +0530

    Add react-modal component and implement ModalComp

--- frontend/src/pages/Vault.jsx
+++ frontend/src/pages/Vault.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Table from "../components/Vault/Table";
 import classes from "../components/Vault/vault.module.css";
+import Modal from "../utils/ModalComp";
 
 function Vault() {
   return (
     <section className={classes.container}>
       <h1>My Vault.</h1>
+      <Modal />
       <Table />
     </section>
   );
