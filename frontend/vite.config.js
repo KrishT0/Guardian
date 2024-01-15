a377f777d56f6d2a2cd6ead119b6a0e048869227
Krishna-windows <krshnabiswal619@gmail.com>
2024-01-16 02:08:58 +0530

    Update server configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    browser: true,
+    open: true, 
     port: 3000,
   },
   plugins: [react()],
