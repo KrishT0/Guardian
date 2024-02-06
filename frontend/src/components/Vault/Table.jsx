94c2416a8b6171c2d5a23816af34634062beac9f
Krishna-windows <krshnabiswal619@gmail.com>
2024-02-06 10:50:00 +0530

    Add react-table dependency and update Table component

--- frontend/src/components/Vault/Table.jsx
+++ frontend/src/components/Vault/Table.jsx
@@ -1,38 +1,10 @@
-import React from "react";
-import classes from "./vault.module.css";
-import List from "./List";
+import React from 'react';
+import classes from './vault.module.css';
+import List from './List';
+import tableData from './data.json';
 
 function Table() {
-  const data = [
-    {
-      id: 1,
-      name: "John Doe",
-      email: "jogndoe@gmail.com",
-      website: "www.johndoe.com",
-      password: "hola1234",
-    },
-    {
-      id: 2,
-      name: "William Brown",
-      email: "williamb@gmail.com",
-      website: "www.johndoe.com",
-      password: "hola1234",
-    },
-    {
-      id: 3,
-      name: "Mary Smith",
-      email: "marrysmith@gmail.com",
-      website: "www.johndoe.com",
-      password: "hola1234",
-    },
-    {
-      id: 4,
-      name: "Steve Rogers",
-      email: "steverog@gmail.com",
-      website: "www.johndoe.com",
-      password: "hola1234",
-    },
-  ];
+  const data = tableData;
 
   return (
     <main>
